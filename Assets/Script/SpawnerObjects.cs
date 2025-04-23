11cb4476a804487e59043b346a1c491e45084d4c
LorenzoPrince <lorenzoprince222@gmail.com>
2025-04-23 19:30:59 -0300

    Coin 3D
    Hice un modelado para la moneda del juego

--- Assets/Script/SpawnerObjects.cs
+++ Assets/Script/SpawnerObjects.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnerObjects : MonoBehaviour
 {
@@ -7,10 +7,13 @@ public class SpawnerObjects : MonoBehaviour
     [SerializeField] GameObject[] blocks;
     public Transform spawnPoint;
 
+
     void Start()
     {
         SpawnBlock();
         InvokeRepeating("SpawnBlock", 0, 6f);
+        Invoke("ChangeSpawnBlock2", 20f);
+        Invoke("SpeedUpSpawnBlock2", 30f);
     }
 
     // Update is called once per frame
@@ -20,9 +23,30 @@ public class SpawnerObjects : MonoBehaviour
     }
     public void SpawnBlock()
     {
-        int randomIndex = Random.Range(0, blocks.Length); //elije un numero entre 0 y el tama�o de la array
+        int mitad = blocks.Length / 2;
+        int randomIndex = Random.Range(0, mitad); //elije un numero entre 0 y el tamaño de la array
         GameObject block = Instantiate(blocks[randomIndex], transform.position, Quaternion.identity);
         block.transform.SetParent(transform); // hace que se muevan los hijos tambien.
+    }
+    public void SpawnBlock2()
+    {
+        int mitad = blocks.Length / 2;
+        int randomIndex = Random.Range(mitad, blocks.Length); //elije un numero entre 0 y el tamaño de la array
+        GameObject block = Instantiate(blocks[randomIndex], transform.position, Quaternion.identity);
+        block.transform.SetParent(transform); // hace que se muevan los hijos tambien.
+    }
+    void ChangeSpawnBlock2()
+
+    {
+        Debug.Log("Cambiando a SpawnBlock2");
+        CancelInvoke("SpawnBlock");
+        InvokeRepeating("SpawnBlock2", 0f, 5f);
+    }
+    void SpeedUpSpawnBlock2()
+    {
+        Debug.Log("⚡ Acelerando SpawnBlock2 a cada 3 segundos");
+        CancelInvoke("SpawnBlock2"); // Cancela solo este si estaba corriendo
+        InvokeRepeating("SpawnBlock2", 0f, 3f);
     }
 }
 
