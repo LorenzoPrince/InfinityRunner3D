6e47c8c971bf9a6094563a89824aa6ef01eb3c78
LorenzoPrince <lorenzoprince222@gmail.com>
2025-04-23 00:49:34 -0300

    Object
    Le agregue obstaculos en el mapa.

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -9,10 +9,15 @@ public class PlayerMove : MonoBehaviour
     public float cambioSpeed = 5f; // velocidad hacia adelante
     public float Speed = 1f;
     private float posicionActual;
+    private bool isGrounded;
+    public float fuerzaSalto;
+    public Rigidbody rigidBody;
     void Start()
     {
         posicionActual = actual * distance;
         transform.position = new Vector3(transform.position.x, transform.position.y, posicionActual);
+        rigidBody = GetComponent<Rigidbody>();
+        isGrounded = true;
     }
     // Update is called once per frame
     void Update()
@@ -34,9 +39,23 @@ public class PlayerMove : MonoBehaviour
 
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidBody.AddForce(0f, fuerzaSalto, 0f, ForceMode.Impulse);
+            isGrounded = false;
+        }
         posicionActual = actual * distance;
         Vector3 posicion = new Vector3(transform.position.x, transform.position.y, posicionActual); //agarra la distancia del nuevo vector
         transform.position = Vector3.Lerp(transform.position, posicion, Time.deltaTime * cambioSpeed); //lo mueve al vector
     }
-} 
+    private void OnCollisionEnter(Collision contraLoQueChoque)//metedo de unity para detectar collisiones
+    {
+        Debug.Log("choque contra " + contraLoQueChoque.gameObject.name);
+        if (contraLoQueChoque.gameObject.CompareTag("Pizo"))
+        {
+            isGrounded = true;
+        }
+    }
+}
 
